927b2d2ddc66ba66cd3077911bf89c40f0231483
sather33 <court.dream3@gmail.com>
2019-02-11 15:06:01 +0800

    modify by eslint

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,9 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
@@ -11,13 +11,16 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <style>{`body { margin: 0 } /* custom! */`}</style>
-          <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=3.0' />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=3.0"
+          />
         </Head>
         <body className="custom_class">
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
